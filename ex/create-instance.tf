31187635dbec7abe141e584e19bc6736eb927b35
pavanbairu <pavan.bairu1@gmail.com>
2023-04-25 22:37:15 +0530

    * printing the ami image id * printing the instance public ip

--- ex/create-instance.tf
+++ ex/create-instance.tf
@@ -1,3 +1,12 @@
+data "aws_ami" "centos" {
+  owners = ["973714476881"]
+  name_regex = "Centos-8-DevOps-Practice"
+}
+
+output "ami" {
+  value = data.aws_ami.centos.image_id
+}
+
 resource "aws_instance" "web"{
   ami = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
@@ -5,4 +14,12 @@ resource "aws_instance" "web"{
   tags = {
     Name = "Hello"
   }
+}
+
+output "web_ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "web_ami" {
+  value = aws_instance.web.ami
 }
