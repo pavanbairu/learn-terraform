0555418b8f77b09bc027f8a988ea4bf4a74020a5
pavanbairu <pavan.bairu1@gmail.com>
2023-04-27 12:19:49 +0530

    datasource for security group

--- data-source/main.tf
+++ data-source/main.tf
@@ -6,6 +6,6 @@ data "aws_security_group" "work-station" {
   id = var.security_group
 }
 
-output "display security group of work-station" {
+output "display-security-group-of-work-station" {
   value = data.aws_security_group.work-station
 }
