e9b33be3a2ec37a71e413b9fd37b535bc1286b5c
pavanbairu <pavan.bairu1@gmail.com>
2023-04-27 12:21:13 +0530

    datasource for security group

--- data-source/main.tf
+++ data-source/main.tf
@@ -2,10 +2,10 @@ variable "security_group" {
   default = "allow-all"
 }
 
-data "aws_security_group" "work-station" {
+data "aws_security_group" "selected" {
   id = var.security_group
 }
 
 output "display-security-group-of-work-station" {
-  value = data.aws_security_group.work-station
+  value = data.aws_security_group.selected
 }
